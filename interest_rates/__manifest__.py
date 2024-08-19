90236aa5dee7d8fb952e6eb00deb88ec229fa2ea
Helmut Drewes <helmut.drewes@agrista.com>
2024-08-19 09:28:24 +0200

    [MIG] interest_rates: migrate to 17.0

--- interest_rates/__manifest__.py
+++ interest_rates/__manifest__.py
@@ -1,19 +1,18 @@
-# Copyright 2020 Agrista GmbH (https://agrista.com)
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
+# Copyright 2020 Agrista (https://agrista.com)
 {
-    "name": "Interest Rates",
-    "version": "1.0.1",
-    "author": "Agrista GmbH",
-    "website": "https://github.com/agrista/odoo-interest-rates",
-    "license": "AGPL-3",
-    "category": "Financial Management/Configuration",
-    "summary": "Update the lending rate / interest rate",
-    "depends": ["base", "account"],
-    "data": [
-        "security/ir.model.access.csv",
-        "security/interest.xml",
-        "views/interest_views.xml",
+    'name': 'Interest Rates',
+    'version': '17.0.1.0.1',
+    'author': 'Agrista',
+    'website': 'https://github.com/agrista/odoo-interest-rates',
+    'license': 'AGPL-3',
+    'category': 'Financial Management/Configuration',
+    'summary': 'Update the lending rate / interest rate',
+    'depends': ['base', 'account'],
+    'data': [
+        'security/ir.model.access.csv',
+        'security/interest.xml',
+        'views/interest_views.xml',
+        'views/menus.xml',
     ],
-    "installable": True,
+    'installable': True,
 }
